f4b503ed2a4c4c84e1d447f71f3c422b252e1f94
Narayana <narayana.mca31@gmail.com>
2019-09-08 11:35:02 +0800

    docker edit-1

--- src/main/java/com/example/configclient/ConfigClientApplication.java
+++ src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -1,9 +1,7 @@
 package com.example.configclient;
 
-import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
